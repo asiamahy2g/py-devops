6aa1505f37a33f5e98c49a1fb62e469b161650a7
Emmanuel Asiamah <asiamahy2g@gmail.com>
2024-05-27 18:11:59 +0000

    adding CLI

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
 from devopslib.randomfruit import fruit
 
-print(fruit())
+import fire
+
+
+def meal(beverage):
+  my_fruit = fruit()
+  print(f"Your fruit is {my_fruit} and your beverage is {beverage}")
+  if my_fruit == "cherry":
+      complete_meal = f"Your meal is a {my_fruit} and {beverage}"
+      return complete_meal
+  alternate_meal = f"Your meal is a steak and {beverage}"
+  return alternate_meal
+if __name__ == '__main__':
+  fire.Fire(meal)
